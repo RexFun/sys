4e566be65da945fd73414da7269925866ebd128d
RexFun <oleFun@icloud.com>
2017-03-21 14:56:48 +0800

    新增‘应用管理’模块

---
+++ src/admin/action/AppAction.java
@@ -0,0 +1,120 @@
+package admin.action;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import admin.entity.App;
+import admin.service.AppService;
+import admin.service.PermitService;
+import chok.devwork.BaseController;
+import chok.util.CollectionUtil;
+import factory.Factory;
+
+@Scope("prototype")
+@Controller
+@RequestMapping("/admin/app")
+public class AppAction extends BaseController<App>
+{
+	@Autowired
+	private AppService service;
+	@Autowired
+	private PermitService permitService;
+	
+	@RequestMapping("/add1")
+	public String add1() 
+	{
+		put("queryParams",req.getParameterValueMap(false, true));
+		return "/admin/app/add.jsp";
+	}
+	@RequestMapping("/add2")
+	public void add2(App po) 
+	{
+		try
+		{
+			service.add(po);
+			print("1");
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			print("0:" + e.getMessage());
+		}
+	}
+	
+	@RequestMapping("/del")
+	public void del() 
+	{
+		try
+		{
+			service.del(CollectionUtil.toLongArray(req.getLongArray("id[]", 0l)));
+			result.setSuccess(true);
+		}
+		catch(Exception e)
+		{
+			result.setSuccess(false);
+			result.setMsg(e.getMessage());
+		}
+		printJson(result);
+	}
+	
+	@RequestMapping("/upd1")
+	public String upd1() 
+	{
+		put("po", service.getById(req.getLong("id")));
+		put("queryParams",req.getParameterValueMap(false, true));
+		return "/admin/app/upd.jsp";
+	}
+	@RequestMapping("/upd2")
+	public void upd2(App po) 
+	{
+		try
+		{
+			service.upd(po);
+			print("1");
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			print("0:" + e.getMessage());
+		}
+	}
+
+	@RequestMapping("/getById")
+	public String getById() 
+	{
+		put("po",service.getById(req.getLong("id")));
+		put("queryParams",req.getParameterValueMap(false, true));
+		return "/admin/app/getById.jsp";
+	}
+
+	@RequestMapping("/get")
+	public String get() 
+	{
+		put("queryParams",req.getParameterValueMap(false, true));
+		return "/admin/app/get.jsp";
+	}
+	
+	@RequestMapping("/getJson")
+	public void getJson()
+	{
+		Map<String, Object> m = req.getParameterValueMap(false, true);
+		result.put("total",service.getCount(m));
+		result.put("rows",service.get(m));
+		printJson(result.getData());
+	}
+	
+	@RequestMapping("/getPermitTreeNodes")
+	public void getPermitTreeNodes()
+	{
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("tc_type", "0");
+		List<Object> treeNodes = Factory.getPermitTreeNodes(req.getLong("id"), params);
+		printJson(treeNodes);
+	}
+}
--- src/factory/Factory.java
+++ src/factory/Factory.java
@@ -1,5 +1,11 @@
 package factory;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import admin.entity.Permit;
 import admin.service.MenuService;
 import admin.service.PermitService;
 import chok.devwork.BeanFactory;
@@ -8,4 +14,50 @@ import chok.devwork.BeanFactory;
 public class Factory {
 	public static MenuService getMenuService(){return (MenuService) BeanFactory.getBean("menuService");}
 	public static PermitService getPermitService(){return (PermitService) BeanFactory.getBean("permitService");}
+	
+	/**
+	 * 根据类型获取权限树节点集合
+	 * @param permitId 已关联权限id
+	 * @param type (类型：应用-0/菜单-1/按钮-2/请求-3)
+	 * @return List<Object>
+	 */
+	public static List<Object> getPermitTreeNodesByType(Long permitId, String type)
+	{
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("tc_type", type);
+		return getPermitTreeNodes(permitId, params);
+	}
+	/**
+	 * 获取权限树节点集合
+	 * @param permitId 已关联权限id
+	 * @param params
+	 * @return List<Object>
+	 */
+	public static List<Object> getPermitTreeNodes(Long permitId, Map<String, Object> params)
+	{
+		List<Object> treeNodes = new ArrayList<Object>();
+		if(permitId!=0)
+		{// 所有权限，且标记已选权限
+			Permit selectedPermitObj = getPermitService().getById(permitId);
+			List<Permit> permitData = getPermitService().get(params);
+			for(int i=0; i<permitData.size(); i++)
+			{
+				Permit o = permitData.get(i);
+				if(o.getLong("id") == selectedPermitObj.getLong("id"))
+				{
+					o.set("checked", true);
+				}
+				treeNodes.add(o.getM());
+			}
+		}
+		else
+		{// 所有权限
+			List<Permit> resultData = getPermitService().get(params);
+			for(Permit o : resultData)
+			{
+				treeNodes.add(o.getM());
+			}
+		}
+		return treeNodes;
+	}
 }
